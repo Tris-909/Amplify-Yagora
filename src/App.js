710a756e0dd9c14ed8258308f1a04a25ee3bdcbe
Tri Tran <tranminhtri9090@gmail.com>
2021-05-09 20:11:43 +1000

    Adding NavBar Component with functions and Creating Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,76 @@
 import React from "react";
-import { withAuthenticator, AmplifyTheme } from 'aws-amplify-react';
+import { Auth, Hub } from 'aws-amplify'; 
+import { Authenticator, AmplifyTheme } from 'aws-amplify-react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import MarketPage from './pages/MarketPage';
+import ProfilePage from './pages/ProfilePage';  
+import Navbar from './components/Navbar';
+
 import "./App.css";
 
 class App extends React.Component {
-  state = {};
+  state = {
+    user: null
+  };
+
+  componentDidMount() {
+    this.getUserData();
+    Hub.listen('auth', this, 'onHubCapsule');
+  }
+
+  getUserData = () => {
+    Auth.currentAuthenticatedUser().then((result) => {
+      this.setState({
+        user: result
+      });
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+
+  onHubCapsule = (capsule) => {
+    switch(capsule.payload.event) {
+      case "signIn":
+        console.log('SIGN IN');
+        this.getUserData();
+        break;
+      case "signUp":
+        console.log('SIGNED UP');
+        break;
+      case "signOut":
+        console.log("SIGNED OUT");
+        this.setState({
+          user: null 
+        });
+        break;
+      default: 
+        return;
+    }
+  }
+
+  signOut = () => {
+    return Auth.signOut();
+  }
 
   render() {
-    return <div>App</div>;
+    const { user } = this.state;
+
+    return !user ? (
+      <Authenticator theme={theme} />
+    ) : (
+      <Router>
+        <>  
+          <Navbar user={user}  signOut={this.signOut} />
+          <div className="app-container">
+            <Route component={HomePage} path="/" exact  />
+            <Route component={ProfilePage} path="/profile" />
+            <Route component={({ match }) => <MarketPage marketId={match.params.marketId} />} path="/market/:marketId" />
+          </div>
+        </>
+      </Router>
+    )
   }
 }
 
@@ -27,4 +91,6 @@ const theme = {
   }
 }
 
-export default withAuthenticator(App, true, [], null, theme);
+// export default withAuthenticator(App, true, [], null, theme);
+
+export default App;
--- src/pages/MarketPage.js
+++ src/pages/MarketPage.js
@@ -1,12 +1,12 @@
 import React from "react";
 // import { Loading, Tabs, Icon } from "element-react";
 
-class MarketPage extends React.Component {
-  state = {};
-
-  render() {
-    return <div>MarketPage</div>;
-  }
+const MarketPage = (props) => {
+  return(
+    <div>
+      MarketPage { props.marketId }
+    </div>
+  )
 }
 
 export default MarketPage;
